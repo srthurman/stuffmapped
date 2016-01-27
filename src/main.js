acc2bec6adf3b904167b956186e95f8a6bbcf3d6
srthurman <srthurman@gmail.com>
2016-01-27 22:26:55 +0000

    Add the first react component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //main.js
 
 var React = require('react');
+var Header = require('./components/common/header');
 var TestComponent = require('./components/map/test');
 
-React.render(<TestComponent/>, document.getElementById('map'));
+React.render(<Header/>, document.getElementById('header'));
