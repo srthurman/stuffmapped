fbabf4308a9c2a6467f9194489be20b3ee5088f1
srthurman <srthurman@gmail.com>
2016-01-27 22:18:48 +0000

    Add the first react component

---
+++ src/components/map/test.js
@@ -0,0 +1,12 @@
+// test.js
+var React = require('react');
+
+var Test = React.createClass({
+    render: function() {
+        return (
+            <h1>Hello, Everyone!</h1>
+        );
+    }
+});
+
+module.exports = Test;
---
+++ src/main.js
@@ -0,0 +1,6 @@
+//main.js
+
+var React = require('react');
+var TestComponent = require('./components/map/test');
+
+React.render(<TestComponent/>, document.getElementById('map'));
